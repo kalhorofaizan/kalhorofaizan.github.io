b4f6e892dd489827512488d78527e377da4d5467
kalhorofaizan <kalhorofaizan@gmail.com>
2020-07-28 22:37:42 +0500

    todo app

--- index.js
+++ index.js
@@ -1,83 +1,42 @@
-class Stopwatch {
-    constructor(display, results) {
-        this.running = false;
-        this.display = display;
-        this.results = results;
-        this.laps = [];
-        this.reset();
-        this.print(this.times);
-    }
-    
-    reset() {
-        this.times = [ 0, 0, 0 ];
-    }
-    
-    start() {
-        if (!this.time) this.time = performance.now();
-        if (!this.running) {
-            this.running = true;
-            requestAnimationFrame(this.step.bind(this));
-        }
-    }
-    
-    stop() {
-        this.running = false;
-        this.time = null;
-    }
-
-    restart() {
-        if (!this.time) this.time = performance.now();
-        if (!this.running) {
-            this.running = true;
-            requestAnimationFrame(this.step.bind(this));
-        }
-        this.reset();
-    }
-    
-    step(timestamp) {
-        if (!this.running) return;
-        this.calculate(timestamp);
-        this.time = timestamp;
-        this.print();
-        requestAnimationFrame(this.step.bind(this));
-    }
-    
-    calculate(timestamp) {
-        var diff = timestamp - this.time;
-        // Hundredths of a second are 100 ms
-        this.times[2] += diff / 10;
-        // Seconds are 100 hundredths of a second
-        if (this.times[2] >= 100) {
-            this.times[1] += 1;
-            this.times[2] -= 100;
-        }
-        // Minutes are 60 seconds
-        if (this.times[1] >= 60) {
-            this.times[0] += 1;
-            this.times[1] -= 60;
-        }
-    }
-    
-    print() {
-        this.display.innerText = this.format(this.times);
-    }
-    
-    format(times) {
-        return `\
-${pad0(times[0], 2)}:\
-${pad0(times[1], 2)}:\
-${pad0(Math.floor(times[2]), 2)}`;
-    }
+function addtodo() {
+    value = document.getElementById("todo").value;
+    document.getElementById("todo").value = "";
+    main = document.createElement("div");
+    main.classList.add("row");
+    div1 = document.createElement("div");
+    div1.classList.add("col-sm-0");
+    div1.classList.add("col-0");
+    div1.classList.add("col-lg-2");
+    div1.classList.add("col-md-2");
+    div2 = document.createElement("div");
+    div2.className = "col-sm-8   col-10";
+    div3 = document.createElement("div");
+    div3.className = "todobox mt-3";
+    span1 = document.createElement("span");
+    span1.innerHTML = value;
+    dbt = document.createElement("button");
+    dbt.className = "btn btn-danger ml-auto";
+    dbt.setAttribute("onClick", "deletelist(this)")
+    dbt.innerHTML = "Delete";
+    ebt = document.createElement("button");
+    ebt.className = "btn btn-secondary  ml-2";
+    ebt.innerHTML = "edit";
+    ebt.setAttribute("onClick", "edit(this)")
+    main.appendChild(div1);
+    div3.appendChild(span1);
+    div3.appendChild(dbt);
+    div3.appendChild(ebt);
+    div2.appendChild(div3);
+    main.appendChild(div2);
+    main.appendChild(div1);
+    document.getElementById("todolist").appendChild(main)
 }
 
-function pad0(value, count) {
-    var result = value.toString();
-    for (; result.length < count; --count)
-        result = '0' + result;
-    return result;
+function deletelist(e) {
+    e.parentNode.remove()
 }
 
-
-let stopwatch = new Stopwatch(
-    document.querySelector('.stopwatch'),
-    document.querySelector('.results'));
+function edit(e) {
+    document.getElementById("todo").value = e.parentNode.firstChild.innerHTML;
+    e.parentNode.remove()
+}
